aabd9bf5c65b69c8850d154f0c714ace98ec1bdb
Kostiantyn <ckolenchenko@yukon.cv.ua>
2020-07-05 16:23:43 +0300

    small corrections.

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -24,14 +24,8 @@
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 
-	<link href="{{ asset('sass/open-iconic/font/css/open-iconic-bootstrap.css') }}" rel="stylesheet">
-	<link href="{{ asset('sass/open-iconic/font/css/open-iconic-foundation.css') }}" rel="stylesheet">
-	<link href="{{ asset('sass/app.css') }}" rel="stylesheet">
-
-{{--
-	<script src="{{ asset('js/cnina.js') }}" type="text/javascript" charset="utf-8"></script>
-	<link href="{{ asset('open-iconic/font/css/open-iconic-bootstrap.css') }}" rel="stylesheet" />
---}}
+	<link href="{{ asset('open-iconic/font/css/open-iconic-bootstrap.css') }}" rel="stylesheet">
+	<link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
 </head>
 
