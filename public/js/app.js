87ebdd5e319715d5f061b87d7c5901a6b7118f34
Kostiantyn <ckolenchenko@yukon.cv.ua>
2020-07-05 16:18:48 +0300

    Open iconic compiling

--- public/js/app.js
+++ public/js/app.js
@@ -19328,15 +19328,27 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /***/ }),
 
+/***/ "./resources/sass/open-iconic/font/css/open-iconic-bootstrap.scss":
+/*!************************************************************************!*\
+  !*** ./resources/sass/open-iconic/font/css/open-iconic-bootstrap.scss ***!
+  \************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
 /***/ 0:
-/*!*************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ***!
-  \*************************************************************/
+/*!******************************************************************************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/sass/open-iconic/font/css/open-iconic-bootstrap.scss ***!
+  \******************************************************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(/*! E:\http\c-nina-l7\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! E:\http\c-nina-l7\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! E:\http\c-nina-l7\resources\sass\app.scss */"./resources/sass/app.scss");
+module.exports = __webpack_require__(/*! E:\http\c-nina-l7\resources\sass\open-iconic\font\css\open-iconic-bootstrap.scss */"./resources/sass/open-iconic/font/css/open-iconic-bootstrap.scss");
 
 
 /***/ })
