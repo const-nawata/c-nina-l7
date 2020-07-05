7587ef1b5259b3e55ee5e4239a03571f9bae469b
Kostiantyn <ckolenchenko@yukon.cv.ua>
2020-07-05 16:01:23 +0300

    SASS settings for Open-iconic

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,6 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+	.sass('resources/sass/app.scss', 'public/css')
+	.sass('resources/sass/open-iconic/font/css/open-iconic-bootstrap.scss', 'public/open-iconic/font/css')
+;
