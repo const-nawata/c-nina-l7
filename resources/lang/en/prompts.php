1be921937c11f431941e476626f116f0efc68b0b
Kostiantyn <ckolenchenko@yukon.cv.ua>
2020-07-11 11:14:18 +0300

    Prompts

--- resources/lang/en/prompts.php
+++ resources/lang/en/prompts.php
@@ -2,9 +2,22 @@
 return [
 	'welcome'	=> 'Welcome.',
 
-	'langs'		=> [
+	'langs'	=> [
 		'en'	=> 'eng',
 		'uk'	=> 'укр',
 		'ru'	=> 'рус'
+	],
+
+	'title'	=> [
+		'currency'		=> 'currency',
+		'dashboard'		=> 'dashboard',
+		'edit'			=> 'edit',
+		'home'			=> 'home',
+		'logging-in'	=> 'logging-in',
+		'logging-out'	=> 'logging-out',
+		'registering'	=> 'registering',
+		'user'			=> 'user'
 	]
 ];
+
+//
--- resources/lang/uk/prompts.php
+++ resources/lang/uk/prompts.php
@@ -2,9 +2,20 @@
 return [
 	'welcome' => 'Ласкаво просимо.',
 
-	'langs'		=> [
+	'langs'	=> [
 		'en'	=> 'eng',
 		'uk'	=> 'укр',
 		'ru'	=> 'рус'
+	],
+
+	'title'	=> [
+		'currency'		=> 'валюта',
+		'dashboard'		=> 'налаштування',
+		'edit'			=> 'редагувати',
+		'home'			=> 'додому',
+		'logging-in'	=> 'авторизація',
+		'logging-out'	=> 'вихід',
+		'registering'	=> 'реєстрація',
+		'user'			=> 'користувач'
 	]
 ];
